4c2709bcbf4e1cb23769d8c56c2aa9529d3af9ec
YusufEminAkpinar <yakpinar137@gmail.com>
2024-08-26 23:51:27 +0300

    Backend almost finished, well, that was easy...

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,19 @@ fn guess_info(goal_word: &String, guess: &String) -> [i8; MAX_CHAR]{
     let mut check_arr: [i8; 5] = [0; 5];
     let b_goal = goal_word.as_bytes();
     let b_guess = guess.as_bytes();
-    let mut iterator: usize = 0;
 
-    for (goal_indice, guess_indice) in b_goal.iter().zip(b_guess.iter()){
-        if goal_indice==guess_indice{
-            retarr[iterator] = 1;
-            check_arr[iterator] = 1;
-        }
-        iterator += 1;
-    }
-    
-    iterator = 0;
-    for goal_indice in b_goal.iter(){
-        for guess_indice in b_guess.iter(){
-            
-        }
-        iterator += 1;
+    for i in 0..5{
+        if b_guess[i] == b_goal[i] {retarr[i] = 1;}
     }
 
-    
-
-       
-    
+    for i in 0..5{
+        for j in 0..5{
+            if b_guess[i]==b_goal[j] && b_guess[i]!=b_guess[j] && check_arr[j]==0 {
+                retarr[i] = -1;
+                check_arr[j] = 1;
+            }
+        }
+    }
     return retarr;
 }
 
@@ -47,8 +38,10 @@ fn main_game(goal_word: &String){
 }
 
 fn main() {
-    let goal = String::from("kalem");
-    main_game(&goal.to_lowercase());
-    
-    println!("Hello, world!");
+    let mut goal = String::new();
+    io::stdin()
+        .read_line(&mut goal)
+        .expect("Failed to read line");
+    let b = goal.to_lowercase().as_bytes();
+    main_game(&goal);
 }
